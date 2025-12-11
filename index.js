319dc4d47ca551e39800d8a36ce5ecbc034f75f5
luke <lfairbanks@anamosa.k12.ia.us>
2025-12-11 17:03:17 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+function taxable(state, taxExempt) {
+	if (state === 'IA' && taxExempt === false) {
+		return 'is taxable';
+	}
+	return '';
+}
+
+function isVehicle(hasWheels, canFly, canSwim) {
+	if (hasWheels || canFly || canSwim) {
+		return 'is vehicle';
+	}
+	return '';
+}
+
